ecf4fcf4b20a70271cf9f0c9044a3dd63fe4dc01
html546 <1776787598@qq.com>
2018-02-10 07:43:15 +0800

    静态变量

--- index.php
+++ index.php
@@ -1,17 +1,35 @@
 <?php
 
-class Foo{
-    public static $my_static = "foo";
-    public function staticValue(){
-        return self::$my_static;
+class Test{
+//   普通字段属于每个对象,每个对象的字段都是相互独立的
+//    public $count = 0;
+
+//静态变量只属于类本身，创建多少个对象都只有一个静态变量
+    public static $count = 0;
+    public function __construct()
+    {
+//        不能通过this来访问静态变量
+        self::$count++;
+    }
+    public static function tt(){
+        echo "tttttt<br/>";
+//        this指向当前的对象
+//        静态方法被调用的时候，静态方法不需要通过对象来调用，所以就没有this
+//        静态方法不能调用非静态方法，以及不能使用非静态字段
+//        $this->demo();//这句是错的
+    }
+    public function demo(){
+        echo "demo<br/>";
+        Test::tt();//非静态方法可以使用静态的方法和静态的字段
     }
 }
-//第一种访问静态变量的方式
-echo Foo::$my_static."<br/>";
-//第二种访问静态变量的方式,只在php5.3版本以上才能使用
-$foo = new Foo();
-echo $foo::$my_static."<br/>";
-//修改类的静态变量
-Foo::$my_static = "noxue";
-//输出类的静态变量
-echo $foo::$my_static;
+//第一种调用静态方法的方式
+Test::tt();
+$tt = new Test();
+//第二种调用静态方法的方式
+$tt::tt();
+for ($i=0;$i<10;$i++){
+    $t = new Test();
+    echo $t::$count."<br/>";
+}
+
