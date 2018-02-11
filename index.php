c0f4a02cd726bfe3beb4c60add44767d8aaa3b13
html546 <1776787598@qq.com>
2018-02-11 10:27:10 +0800

    抽象类的创建和继承

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <?php
 
-class Test{
-    public $t = 0;
-    function tt(){
-        $this->t=100;
+abstract class Animal{
+//    抽象方法，必须在子类被实现
+    abstract function eat($something);
+//一般的方法，可以直接在抽象类里面实现，跟一般的类中的方法一样.
+    function run(){
+        echo "行走<br/>";
     }
 }
 
-$t1 = new Test();
-$t1->tt();
+/**
+ * 继承抽象类，实现抽象方法的快捷键,alt+enter
+ * 1.抽象类不能被直接实例化(用来创建对象)
+ * 2.抽象类中的抽象方法必须在子类中实现
+ * Class Cat
+ */
+class Cat extends Animal{
 
-$t2 = new Test();
-$t2->tt();
+    function eat($something)
+    {
+        echo "吃$something<br/>";
+    }
+}
 
+$cat = new Cat();
+$cat->eat("东西");
 
