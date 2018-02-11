d4dbcd12a6a8b049e124767d224fd3a2b7f868d8
html546 <1776787598@qq.com>
2018-02-11 22:04:57 +0800

    trait 的定义和使用

--- index.php
+++ index.php
@@ -1,40 +1,31 @@
 <?php
 
-/**
- * 只能继承一个类，但是可以实现多个接口
- * 定义接口，相当于定义一个标准
- * 接口中的方法必须是public的
- * 用implements 来实现一个接口，如果有多个接口，用逗号分隔开
- * 接口可以继承另一个接口，用extends来继承一个接口
- * Interface IUsb
- */
-interface IUsb{
-    public function write($msg);
-    function read();
-}
-
-/**
- * 可以通过extends继承其他接口
- * Interface IExt
- */
-interface IExt extends IUsb {
-    function other();
-}
-
-class Test implements IExt{
-
-    public function write($msg)
-    {
-        // TODO: Implement write() method.
-    }
-
-    function read()
-    {
-        // TODO: Implement read() method.
-    }
-
-    function other()
-    {
-        // TODO: Implement other() method.
+trait LogTrait{
+    public function log($msg){
+        echo $this->name."的日志:$msg<br/>";
     }
 }
+
+class Test1{
+    public $name = "test1";
+    use LogTrait;
+}
+
+class Test2{
+    public $name = "test2";
+    use LogTrait;
+}
+
+class Test3{
+    public $name = "test3";
+    use LogTrait;
+}
+
+$test1 = new Test1();
+$test1->log('日志信息');
+
+$test2 = new Test2();
+$test2->log('日志信息');
+
+$test3 = new Test3();
+$test3->log('日志信息');
