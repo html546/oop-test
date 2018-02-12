710a39f6f96800930eca1f6d72a56df949b2b5ab
html546 <1776787598@qq.com>
2018-02-12 15:57:26 +0800

    魔术方法__call的用法

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
 
-trait LogTrait{
-    private function log($msg){
-        echo "log日志:$msg<br/>";
+class Test{
+    public function __call($name, $arguments)
+    {
+        echo $name;
+        var_dump($arguments);
     }
 }
 
-class Test {
-    use LogTrait{log as public;}
-}
-
 $t = new Test();
-$t->log("日志信息");
+$t->admin(1,2,3,'xxxx');
