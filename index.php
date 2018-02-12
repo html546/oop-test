625ef00458415b61f12f706dc34c3f2ca915f1c9
html546 <1776787598@qq.com>
2018-02-12 09:49:26 +0800

    trait 优先级[2C

--- index.php
+++ index.php
@@ -2,42 +2,19 @@
 
 trait LogTrait{
     public function log($msg){
-        echo $this->name."的日志:$msg<br/>";
-    }
-}
-
-trait sqlError{
-    public $sql = "select * from xxxx;";
-    public function error(){
-        echo $this->name."sql语句错误了<br/>";
+        echo "日志:$msg<br/>";
     }
 }
 
 class Test1{
-    public $name = "test1";
-    use LogTrait,sqlError;
-    public function test(){
-        echo $this->sql;
+    function log($msg){
+        echo "父类(基类)的log方法被调用<br/>";
     }
 }
 
-class Test2{
-    public $name = "test2";
+class Test2 extends Test1{
     use LogTrait;
 }
 
-class Test3{
-    public $name = "test3";
-    use LogTrait;
-}
-
-$test1 = new Test1();
-$test1->log('日志信息');
-$test1->error();
-$test1->sql;
-
-$test2 = new Test2();
-$test2->log('日志信息');
-
-$test3 = new Test3();
-$test3->log('日志信息');
+$t = new Test2();
+$t->log("日志信息");
