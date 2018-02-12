9c5184f3a2f1042905caadf667623c5c62d7b5f1
html546 <1776787598@qq.com>
2018-02-12 21:34:42 +0800

    匿名类的使用

--- index.php
+++ index.php
@@ -1,28 +1,21 @@
 <?php
 
-final class Animal{
-//    如果类前面加了final，则里面的方法，加不加final都无所谓
-//因为已经不能被继承了，所以肯定也不能被覆盖了.
-    final public function run(){
-        echo "Animal run<br/>";
-    }
-    public function eat(){
-        echo "Animal eat<br/>";
-    }
-}
-//以下是错误写法,因为Animal类用final修饰后就不能被继承
-class Person extends Animal{
-    public function eat()
-    {
-        echo "person eat<br/>";
-    }
-
-    /**
-     * 以下是错误写法，因为final修饰的方法不能被子类覆盖
-     */
-//    public function run(){
-//        echo "person run<br/>";
-//    }
+function logInfo($class){
+    $class->log("日志信息");
 }
 
+class Loger{
+    function log($msg){
+        echo $msg;
+    }
+}
 
+$l = new Loger;
+//第一种方法，不适用匿名类
+//logInfo($l);
+//第二种方法,创建匿名类.
+logInfo(new class{
+    function log($msg){
+        echo $msg;
+    }
+});
