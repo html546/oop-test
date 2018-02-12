0cd0cdc0e830696fa666437305f62868cf5e61a1
html546 <1776787598@qq.com>
2018-02-12 20:48:08 +0800

    类前面加final的类不能被继承

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-class Animal{
+final class Animal{
+//    如果类前面加了final，则里面的方法，加不加final都无所谓
+//因为已经不能被继承了，所以肯定也不能被覆盖了.
     final public function run(){
         echo "Animal run<br/>";
     }
@@ -8,7 +10,7 @@ class Animal{
         echo "Animal eat<br/>";
     }
 }
-
+//以下是错误写法,因为Animal类用final修饰后就不能被继承
 class Person extends Animal{
     public function eat()
     {
