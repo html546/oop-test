e90c77a3c039d8b9dd4eecb8e0cc045a97636616
html546 <1776787598@qq.com>
2018-02-12 16:49:21 +0800

    __toString 函数的用法

--- index.php
+++ index.php
@@ -1,19 +1,31 @@
 <?php
 
-class Test{
-    public function __call($name, $arguments)
+class Person{
+    public $name;
+    private $sex;
+    public function __construct($name,$sex)
     {
-        echo "$name 页面不存在";
+        $this->name=$name;
+        $this->sex=$sex;
     }
-    function index(){
-        echo "index<br/>";
-    }
-    function show(){
-        echo "show<br/>";
-    }
+
+//    function show(){
+//        echo "姓名:".$this->name." 性别:".$this->sex;
+//    }
+
+    /**
+     * 方法用于一个类被当成字符串时应怎样回应。
+     * 例如 echo $obj;应该显示些什么。
+     * 此方法必须返回一个字符串，
+     * 否则将发出一条E_RECOVERABLE_ERROR级别的致命错误
+     * @return string
+     */
+    public function __toString()
+   {
+        return "姓名:".$this->name." 性别:".$this->sex;
+   }
 }
 
-$t = new Test();
-$t->index();
-$t->show();
-$t->shoq();
+$person = new Person('张三','男');
+
+echo $person;
