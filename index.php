e40828baa768ad8c4b843331bded29b96733f2c4
html546 <1776787598@qq.com>
2018-02-12 18:33:13 +0800

    遍历对象

--- index.php
+++ index.php
@@ -1,31 +1,25 @@
 <?php
 
 class Person{
-    public $name;
-    private $sex;
-    public function __construct($name,$sex)
+    public $name = "张三";
+    private $sex="男";
+    protected $age = 123;
+    public function __construct()
     {
-        $this->name=$name;
-        $this->sex=$sex;
+    }
+    public function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+    }
+    function Test(){
+        echo "test";
     }
 
-//    function show(){
-//        echo "姓名:".$this->name." 性别:".$this->sex;
-//    }
-
-    /**
-     * 方法用于一个类被当成字符串时应怎样回应。
-     * 例如 echo $obj;应该显示些什么。
-     * 此方法必须返回一个字符串，
-     * 否则将发出一条E_RECOVERABLE_ERROR级别的致命错误
-     * @return string
-     */
-    public function __toString()
-   {
-        return "姓名:".$this->name." 性别:".$this->sex;
-   }
 }
 
-$person = new Person('张三','男');
 
-echo $person;
+$person = new Person();
+//遍历对象中的可见属性(也就是public权限的字段)
+foreach ($person as $k=>$v){
+    echo "$k ----- $v<br/>";
+}
