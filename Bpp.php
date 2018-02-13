8abe37abe0383059ed01ec35767a7e17573e24bb
html546 <1776787598@qq.com>
2018-02-13 11:35:49 +0800

    别名的使用

---
+++ Bpp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DELL
+ * Date: 2018/2/13
+ * Time: 11:22
+ */
+//定义命名空间
+namespace Bpp;
+
+class Person{
+    function Test(){
+        echo "test";
+    }
+}
--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 //就算使用命名空间，依然需要先引入php文件，否则找不到其中的类
 require_once "App.php";
-//引入命名空间中的类
-use App\Person;
+require_once "Bpp.php";
+//引入命名空间中的类,并取别名,别名防止冲突
+use App\Person as Aperson;
+use Bpp\Person as Bperson;
 
-$person = new Person();
+$person = new Aperson();
 $person->Test();
