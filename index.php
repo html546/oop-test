0ba57fada841cb10b34e38f9284586188ab88b6a
html546 <1776787598@qq.com>
2018-02-13 11:30:17 +0800

    命名空间的定义和使用

--- index.php
+++ index.php
@@ -1,26 +1,8 @@
 <?php
+//就算使用命名空间，依然需要先引入php文件，否则找不到其中的类
+require_once "App.php";
+//引入命名空间中的类
+use App\Person;
 
-class Person{
-    public $name;
-    public function __construct($name)
-    {
-        $this->name=$name;
-    }
-
-    function show(){
-        echo "姓名:".$this->name;
-    }
-}
-
-/**
- * 类型约束的意义在于，只有参数类型与约束的类型一样的时候，才能调用成功
- * @param Person $person
- */
-function test(Person $person){
-    $person->show();
-}
-
-$person = new Person('张三');
-test($person);
-//此处传1进去，类型和Person类不一样，所以会报错
-//test(1);
+$person = new Person();
+$person->Test();
