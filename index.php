c9650a3aeed5f58c7457bca389ba23fc2701b7a5
html546 <1776787598@qq.com>
2018-02-13 12:20:09 +0800

    自动加载，命名空间名称与目录名必须一样，类名与文件名一样

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 //就算使用命名空间，依然需要先引入php文件，否则找不到其中的类
-require_once "App.php";
-require_once "Bpp.php";
+spl_autoload_register(function($name){
+    require_once "$name.php";
+});
 //引入命名空间中的类,并取别名,别名防止冲突
 use App\Person as Aperson;
 use Bpp\Person as Bperson;
