1f7dc3e317186a92422d5823e52e1d4066dc8956
html546 <1776787598@qq.com>
2018-02-14 10:21:29 +0800

    异常处理2 自定义异常处理

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 
+class NameEmptyException extends Exception {
+
+}
+
+class PassEmptyException extends Exception {
+
+}
+
 function login($name,$pass){
     if(empty($name)){
-        throw new Exception('用户名不能为空');
+        throw new NameEmptyException('用户名不能为空');
     }
     if(empty($pass)){
-        throw new Exception('密码不能为空');
+        throw new PassEmptyException('密码不能为空');
     }
 
     echo "登录成功";
@@ -13,6 +21,8 @@ function login($name,$pass){
 }
 try{
     login($_GET['name'],$_GET['pass']);
-}catch (Exception $e){
-    echo $e->getMessage();
+}catch (NameEmptyException $e){
+    echo "用户名不能为空";
+}catch (PassEmptyException $e){
+    echo "密码不能为空";
 }
