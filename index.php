1a94ccec1c72de90496bfae3803c56a767621883
html546 <1776787598@qq.com>
2018-02-14 10:27:32 +0800

    异常案例3

--- index.php
+++ index.php
@@ -1,19 +1,13 @@
 <?php
 
-class NameEmptyException extends Exception {
 
-}
-
-class PassEmptyException extends Exception {
-
-}
 
 function login($name,$pass){
     if(empty($name)){
-        throw new NameEmptyException('用户名不能为空');
+        throw new Exception('user',1);
     }
     if(empty($pass)){
-        throw new PassEmptyException('密码不能为空');
+        throw new Exception('pass',2);
     }
 
     echo "登录成功";
@@ -21,8 +15,10 @@ function login($name,$pass){
 }
 try{
     login($_GET['name'],$_GET['pass']);
-}catch (NameEmptyException $e){
-    echo "用户名不能为空";
-}catch (PassEmptyException $e){
-    echo "密码不能为空";
+}catch (Exception $e){
+    if($e->getCode()==1){
+        echo "用户名不能为空";
+    }elseif ($e->getCode()==2){
+        echo "密码不能为空";
+    }
 }
