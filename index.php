425061432863ec5d0708c89e709a8ae23f4f88cc
html546 <1776787598@qq.com>
2018-02-14 09:24:00 +0800

    自定义异常类，捕获异常

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
+//继承一个Exception来自定义一个异常类
+class OtherException extends Exception{
 
-spl_autoload_register(function($name){
-    require_once "$name.php";
-});
-//完全限定类名 = 当前命名空间 + 限定类名
-// A\B\B       = A\B          +B
-//完全限定类名:相当于文件的绝对路径
-//当前命名空间:相当于文件所在文件夹路径
-//限定类名:相当于文件名
-$b = new A\B\B();
+}
+
+
+function test(){
+//    用throw 关键字抛出一个异常对象
+    throw new OtherException('这里发生了异常');
+}
+
+try{
+    test();
+}catch (OtherException $e){//自定义的Exception放在前面捕获
+    echo "OtherException:".$e->getMessage();
+}catch (Exception $e){
+    echo "Exception:".$e->getMessage();
+}
