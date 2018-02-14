c03282beb0c95793ee0d9a734e7ab3a445ecf444
html546 <1776787598@qq.com>
2018-02-14 10:14:44 +0800

    异常处理案例1

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
 
+function login($name,$pass){
+    if(empty($name)){
+        throw new Exception('用户名不能为空');
+    }
+    if(empty($pass)){
+        throw new Exception('密码不能为空');
+    }
+
+    echo "登录成功";
+
+}
 try{
-    $dns = "mysql:host=localhost;dbname=qy";
-    $db = new PDO($dns,'root','root');
-    $db->exec("set names utf8");
+    login($_GET['name'],$_GET['pass']);
 }catch (Exception $e){
     echo $e->getMessage();
-}finally{
-
 }
