20ae5d9bc6b0c96c3c09c5786a1f15e538ef0e90
html546 <1776787598@qq.com>
2018-02-14 10:00:16 +0800

    判断数据库连接是否成功

--- index.php
+++ index.php
@@ -1,21 +1,11 @@
 <?php
-//继承一个Exception来自定义一个异常类
-class OtherException extends Exception{
-
-}
-
-
-function test(){
-//    用throw 关键字抛出一个异常对象
-    throw new OtherException('这里发生了异常');
-}
 
 try{
-    test();
-}catch (OtherException $e){//多种异常可以在一个catch语句中处理,用|分隔
-    echo $e->getMessage();
+    $dns = "mysql:host=localhost;dbname=qy";
+    $db = new PDO($dns,'root','root');
+    $db->exec("set names utf8");
 }catch (Exception $e){
     echo $e->getMessage();
 }finally{
-    echo "不管是否有异常，这里的代码总是会被执行";
+
 }
