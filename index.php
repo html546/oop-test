09d576c759012d0fa4e91b0ce83a95b4b0e3e2de
html546 <1776787598@qq.com>
2018-02-16 08:39:36 +0800

    案例一

--- index.php
+++ index.php
@@ -1,24 +1,48 @@
 <?php
 
+//这个类用来表示一个点
+class Point{
+    public $x;
+    public $y;
 
-
-function login($name,$pass){
-    if(empty($name)){
-        throw new Exception('user',1);
-    }
-    if(empty($pass)){
-        throw new Exception('pass',2);
-    }
-
-    echo "登录成功";
-
-}
-try{
-    login($_GET['name'],$_GET['pass']);
-}catch (Exception $e){
-    if($e->getCode()==1){
-        echo "用户名不能为空";
-    }elseif ($e->getCode()==2){
-        echo "密码不能为空";
+    public function __construct($x=0,$y=0)
+    {
+        $this->x = $x;
+        $this->y = $y;
     }
 }
+
+class Map{
+    public $poits;
+}
+
+class Person{
+    public $point;
+
+//    人物初始位置
+    public function __construct(Point $point)
+    {
+        $this->point=$point;
+    }
+
+    /**
+     * 人物移动到某个点
+     * @param Point $point
+     */
+    function move(Point $point){
+        $this->point = $point;
+    }
+    public function __toString()
+    {
+        return "x:".$this->point->x." y:".$this->point->y."<br/>";
+    }
+}
+//初始化人的位置
+$start = new Point();
+$person = new  Person($start);
+
+echo "当前人的位置: ".$person;
+//移动人到新的位置
+$end = new Point(100,200);
+$person->move($end);
+echo "当前人的位置 :".$person;
